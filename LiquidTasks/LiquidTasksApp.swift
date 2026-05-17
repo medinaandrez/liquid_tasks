e2f4a2e6a456bbd7c97e228c3f19de4dea3d975e
Andres Medina <medinaandrez@gmail.com>
2026-05-17 19:01:48 -0400

    Initial Commit

---
+++ LiquidTasks/LiquidTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidTasksApp.swift
+//  LiquidTasks
+//
+//  Created by Andres Medina on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LiquidTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
